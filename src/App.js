f35a036c2b690fe45570a5b4b2c4f3b6895c3108
azimjonking <azimjonjalilov1301@mail.ru>
2024-07-03 10:54:38 +0300

    login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
+import Header from "./components/header/Header";
+import Login from "./components/login/Login";
+import Home from "./components/routes/home/Home";
 
 function App() {
+  const [isLogin, setIsLogin] = useState(true);
   return (
     <div className="App">
-      <h1>Algoritm Coin</h1>
+      {isLogin && <Header />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="container">
+      <h1>Header</h1>
+    </header>
+  );
+};
+
+export default Header;
