4a6531d499d9363cc2bd6ace22c0af791358ea4c
azimjonking <azimjonjalilov1301@mail.ru>
2024-07-24 09:59:49 +0300

    students list add

--- src/components/studentsList/StudentsList.js
+++ src/components/studentsList/StudentsList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import "./StudentsList.css";
+import { TiTick } from "react-icons/ti";
 
 const StudentsList = () => {
   const [students, setStudents] = useState([]);
@@ -24,10 +25,12 @@ const StudentsList = () => {
       <table className="students-table">
         <thead>
           <tr>
+            <td>№</td>
             <td>Ism / Familiya</td>
             <td>Telefon Raqam</td>
-            <td className='subjects'>
-              <select name="" id="">
+            <td className="subjects">
+              <select name="subject" id="subject">
+                <option value="all">All</option>
                 <option value="english">English</option>
                 <option value="IT">IT</option>
                 <option value="fizika">Fizika</option>
@@ -36,40 +39,47 @@ const StudentsList = () => {
             </td>
             <td>Coin qo'shish</td>
             <td>Coins</td>
-            <td className='days'>
-              <select name="" id="">
+            <td className="days">
+              <select name="day" id="day">
                 <option value="all">All Days</option>
                 <option value="toq">toq kunlar</option>
                 <option value="juft">juft kunlar</option>
               </select>
             </td>
-            <td>Action</td>
-            <td>Action</td>
+            <td className="action">Action</td>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Utkir Giyosov</td>
-            <td>+998 90 694 8717</td>
-            <td>Ingliz tili</td>
-            <td>
-              <select name="" id="">
-                <option value="null"></option>
+          {students &&
+            students.map((student, i) => {
+              return (
+                <tr>
+                  <td>{i+1}</td>
+                  <td>{student.firstName} {student.lastName}</td>
+                  <td>{student.phone}</td>
+                  <td>{student.subjects[0].subject}</td>
+                  <td className="add-coin">
+                    {/* <select name="coin" id="coin">
+                <option value="null">Coin tanlang</option>
                 <option value="3">3</option>
                 <option value="5">5</option>
                 <option value="7">7</option>
                 <option value="10">10</option>
-              </select>
-            </td>
-            <td>7 000</td>
-            <td>14-16</td>
-            <td>
-              <Link>O'zgartirish</Link>
-            </td>
-            <td>
-              <button>O'chirish</button>
-            </td>
-          </tr>
+              </select> */}
+                    <input type="text" />
+                    <button>
+                      <TiTick className="icon" />
+                    </button>
+                  </td>
+                  <td>7 000</td>
+                  <td>14-16</td>
+                  <td className="action">
+                    <Link>Edit</Link>
+                    <button>Delete</button>
+                  </td>
+                </tr>
+              );
+            })}
         </tbody>
       </table>
     </div>
